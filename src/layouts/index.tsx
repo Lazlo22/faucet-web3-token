aab03b625666d0cd55af5b79d70c526cd7363897
Lazlo22 <v.prihodski@gmail.com>
2025-01-24 12:46:59 +0100

    fix: layout styles fix

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header/index";
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
     return (
-        <div className="size-full tracking-wide flex items-center justify-center flex-col gap-12">
+        <div className="size-full tracking-wide px-24 flex items-center justify-center flex-col gap-12">
             <Header />
             {children}
         </div>
