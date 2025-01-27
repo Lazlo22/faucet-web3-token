ac97f4528cfe687f2669c107dbc5899839324625
Lazlo22 <v.prihodski@gmail.com>
2025-01-27 21:48:32 +0100

    feat: Faucet form address validation

--- src/components/Faucet/FaucetForm/index.tsx
+++ src/components/Faucet/FaucetForm/index.tsx
@@ -1,8 +1,15 @@
-import {FC, FormEvent} from "react";
+import {ChangeEvent, FC, FormEvent, useState} from "react";
+import { isAddress } from "viem";
 
 const FaucetForm: FC = () => {
+    const [userAddress, setUserAddress] = useState<string>('');
+
     const handleSubmitFaucet = (e: FormEvent) => {
         e.preventDefault();
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUserAddress(e.target.value);
     };
 
     return (
@@ -11,11 +18,18 @@ const FaucetForm: FC = () => {
                 <input
                     type="text"
                     id="address"
-                    class="w-[30rem] bg-gray-50 border border-2 border-gray-300 shadow-md text-gray-900 text-md placeholder-gray-400 rounded-lg outline-none hover:bg-gray-100 focus:ring-black focus:border-black block p-3.5"
+                    name="address"
+                    className="w-[30rem] bg-gray-50 border border-2 border-gray-300 shadow-md text-gray-900 text-md placeholder-gray-400 rounded-lg outline-none hover:bg-gray-100 focus:ring-black focus:border-black block p-3.5"
                     placeholder="Enter your wallet address (0x...)"
+                    value={userAddress}
+                    onChange={handleChange}
                     required
                 />
-                <button type="submit" class="text-white bg-black hover:bg-black/[0.85] outline-none focus:ring-4 focus:ring-pink-700/[0.7] font-semibold rounded-lg text-lg w-auto px-5">
+                <button
+                    type="submit"
+                    className="text-white bg-black enabled:hover:bg-black/[0.85] outline-none transform enabled:active:scale-[0.8] transition-transform font-semibold rounded-lg text-lg w-auto px-5 disabled:opacity-50"
+                    disabled={!isAddress(userAddress)}
+                >
                     Get Tokens
                 </button>
             </div>
