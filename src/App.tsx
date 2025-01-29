69ce8797dad08e89bf1fd0f574ad2186f1e43976
Lazlo22 <v.prihodski@gmail.com>
2025-01-29 09:07:24 +0100

    feat: Added React toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+
 import MainLayout from "./layouts/index";
 import Title from "./components/Title/index";
 import ReownProvider from "./components/ReownProvider/index";
@@ -7,12 +9,15 @@ import './App.css';
 
 function App() {
     return (
-        <ReownProvider>
-            <MainLayout>
-                <Title/>
-                <Faucet />
-            </MainLayout>
-        </ReownProvider>
+        <>
+            <ReownProvider>
+                <MainLayout>
+                    <Title/>
+                    <Faucet />
+                </MainLayout>
+            </ReownProvider>
+            <ToastContainer position='top-center' />
+        </>
     );
 }
 
